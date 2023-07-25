836ec4ec6eede20b4dd7b0b1f87357c0f42fc928
Julian Köhler <julkoehl@icloud.com>
2023-07-25 17:49:48 +0200

    Added landing page. Project finished for deployment

---
+++ lib/testimonials.ts
@@ -0,0 +1,26 @@
+export const testimonials = [
+  {
+    name: "Antonio",
+    avatar: "A",
+    title: "Software Engineer",
+    description: "This is the best toolbox I've used so far!",
+  },
+  {
+    name: "Antonio",
+    avatar: "A",
+    title: "Software Engineer",
+    description: "This is the best toolbox I've used so far!",
+  },
+  {
+    name: "Antonio",
+    avatar: "A",
+    title: "Software Engineer",
+    description: "This is the best toolbox I've used so far!",
+  },
+  {
+    name: "Antonio",
+    avatar: "A",
+    title: "Software Engineer",
+    description: "This is the best toolbox I've used so far!",
+  },
+];
