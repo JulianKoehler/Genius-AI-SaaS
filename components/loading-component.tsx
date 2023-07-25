1c4f205b090020107112c262058d78083728973d
Julian Köhler <julkoehl@icloud.com>
2023-07-25 18:23:35 +0200

    Removed loader-spinner import in loading component

--- components/loading-component.tsx
+++ components/loading-component.tsx
@@ -1,7 +1,6 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
-import { ThreeDots } from "react-loader-spinner";
 
 type Props = {
   imagePage?: boolean;
